07baae8f1b34274236dd92c4e459814d898ac88b
Alex Pin <ronnyaalez@gmail.com>
2026-02-25 12:56:05 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const ratings = document.querySelectorAll('#star');
-let selectedRating = null;
+const Ratings = document.querySelectorAll('.star');
+let selectedRating = 0;
 
-const submitButton = document.querySelector('#Submit');
+const submitButton = document.getElementById('#Submit');
 
-ratings.forEach((button) => {
-button.addEventListener('click', () => {
-    ratings.forEach((btn) => btn.classList.remove('selected'));
-    button.classList.add('selected'); })
+Ratings.forEach((button) => {
+    button.addEventListener('click', () => {
+        Ratings.forEach((btn) => btn.classList.remove('selected'));
+        button.classList.add('selected');
+        selectedRating = button.getAttribute('data-rating');
+    });
+
 })
