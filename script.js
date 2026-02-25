f57fdb29329c97ba5964f77613f106d74eaf4729
Alex Pin <ronnyaalez@gmail.com>
2026-02-25 13:18:34 +0100

    update script.js and index.html

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const Ratings = document.querySelectorAll('.star');
-let selectedRating = 0;
+let selectedRating = null;
 
-const submitButton = document.getElementById('#Submit');
+const submitButton = document.querySelector('#Submit');
 
 Ratings.forEach((button) => {
     button.addEventListener('click', () => {
         Ratings.forEach((btn) => btn.classList.remove('selected'));
         button.classList.add('selected');
-        selectedRating = button.getAttribute('data-rating');
+        selectedRating = button.textContent;
     });
 
+})
+
+
+submitButton.addEventListener('click', () => {
+document.querySelector('#rating-display').textContent = selectedRating;
+document.querySelector('#rating-card').style.display = 'none'
+document.querySelector('#Thank-you-card').style.display = 'block'
 })
